d69c46fa6eed50ad6f2183584cf949b3ba9d1cb8
rzryr1 <solotogre@gmail.com>
2024-12-03 16:15:51 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+using namespace std;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+int main() {
+    double a  =  20; 
+    double b = 30;
+    double c = 4, output;
+
+    output = a - b/c;
+    cout << "output: " << output ;
     return 0;
 }
